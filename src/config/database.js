2f733211eadcd30a7af15fb2efb6780c6e89faea
Willian Daniel Calefi <willian.calefi@prefeitura.araras>
2020-08-13 13:52:41 -0300

    implementação MVC e migration(sequelize)

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'mrjl4head_pma',
+    database: 'sqlnode',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+};
